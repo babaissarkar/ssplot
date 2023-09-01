31fca6c57c68c6078e09cc857a269aeadf2f740f
babaissarkar <suvrax@gmail.com>
2023-09-01 11:40:17 +0530

    Deb file and its sources added.

--- generate_all.sh
+++ generate_all.sh
@@ -2,6 +2,10 @@
 echo "Running Maven"
 mvn package
 echo "Copying target jar"
-cp -v target/SSPlot.jar jar/
+cp -v target/SSPlot-2.0-shaded.jar jar/SSPlot.jar
+rm -v ssplot-deb/usr/share/java/SSPlot.jar
+cp -v jar/SSPlot.jar ssplot-deb/usr/share/java
 echo "Generating Docs"
 markdown README.md > README.html
+echo "Generating DEB package"
+dpkg-deb -b ssplot-deb
