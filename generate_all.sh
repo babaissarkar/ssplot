17942e20417d415cf0b960603a74ce34b926b593
Subhraman Sarkar <suvrax@gmail.com>
2024-10-20 18:50:11 +0530

    update buildfiles: generate DEB package with maven

--- generate_all.sh
+++ generate_all.sh
@@ -5,18 +5,10 @@
 # should exist on PATH
 # build-deps : maven, dpkg-deb, appimagetool, markdown
 VERSION=2.2.2
-echo "Running Maven"
-mvn package
-echo "Copying target jar"
-rm -v "jar/SSPlot.jar"
-cp -v "target/ssplot.jar" "jar/SSPlot.jar"
-rm -v ssplot-deb/usr/share/java/SSPlot*.jar
-cp -v jar/SSPlot.jar ssplot-deb/usr/share/java
+echo "Running Maven: Compile and generate DEB"
+mvn clean package -X
 echo "Generating Docs"
 markdown README.md > README.html
-echo "Generating DEB package"
-dpkg-deb -b ssplot-deb
-mv -v "ssplot-deb.deb" "ssplot-${VERSION}.deb"
 echo "Creating AppImage"
 cp -v "jar/SSPlot.jar" "SSPlot.AppDir/usr/share/ssplot/ssplot.jar"
 ARCH=x86_64 appimagetool "SSPlot.AppDir"
