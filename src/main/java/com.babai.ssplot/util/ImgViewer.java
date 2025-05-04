c02ff26905bc976b1a32525e3fb1dffbdc0a3852
Subhraman Sarkar <suvrax@gmail.com>
2025-05-04 21:17:41 +0530

    ImgViewer: private ctor, new static method for viewing

--- src/main/java/com.babai.ssplot/util/ImgViewer.java
+++ src/main/java/com.babai.ssplot/util/ImgViewer.java
@@ -1,26 +1,32 @@
 package util;
 
-import javax.swing.*;
-import java.awt.*;
 import java.awt.image.BufferedImage;
+
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class ImgViewer {
-    private JFrame frmMain;
-    private JLabel lblView;
+	private JFrame frmMain;
+	private JLabel lblView;
 
-    public ImgViewer() {
-        frmMain = new JFrame("Preview");
-        lblView = new JLabel();
-        lblView.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-        lblView.setHorizontalAlignment(JLabel.CENTER);
-        frmMain.add(lblView);
-    }
+	private ImgViewer() {
+		frmMain = new JFrame("Preview");
+		lblView = new JLabel();
+		lblView.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		lblView.setHorizontalAlignment(JLabel.CENTER);
+		frmMain.add(lblView);
+	}
 
-    public void show(BufferedImage img) {
-        frmMain.repaint();
-        lblView.setIcon(new ImageIcon(img));
-        frmMain.setVisible(true);
-        frmMain.pack();
-    }
+	private void showImpl(BufferedImage img) {
+		frmMain.repaint();
+		lblView.setIcon(new ImageIcon(img));
+		frmMain.setVisible(true);
+		frmMain.pack();
+	}
+
+	public static void show(BufferedImage img) {
+		new ImgViewer().showImpl(img);
+	}
 }
