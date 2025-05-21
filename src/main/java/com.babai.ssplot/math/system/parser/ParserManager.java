1f60e6b69bab65995b78d5e41a46239f8bbca05a
Subhraman Sarkar <suvrax@gmail.com>
2025-05-21 21:08:09 +0530

    Remove InternalParser, make TreeParser implement Parser

--- src/main/java/com.babai.ssplot/math/system/parser/ParserManager.java
+++ src/main/java/com.babai.ssplot/math/system/parser/ParserManager.java
@@ -1,15 +1,51 @@
 package math.system.parser;
 
-import java.util.Map;
+import java.util.HashMap;
+
+import javax.script.ScriptEngineManager;
+
+import math.system.parser.internal.tree.TreeParser;
 
 public class ParserManager {
-	private Map<String, Parser> parsers;
+	private HashMap<String, Parser> parsers;
+	private Parser currParser = null;
 	
 	public ParserManager() {
+		parsers = new HashMap<>();
 		
+		populateEngineList();
+		
+		// Current engine selection
+		// Engine set via envvar
+		String engineName = System.getenv("SSPLOT_ENGINE");
+		if (engineName.isEmpty()) {
+			currParser = parsers.get(engineName);
+		} else {
+			currParser = parsers.get("Internal"); // TODO Magic string
+		}
 	}
 	
-	public Map<String, Parser> availableParsers() {
+	public HashMap<String, Parser> availableParsers() {
 		return parsers;
+	}
+	
+	public Parser getParser() {
+		return currParser;
+	}
+	
+	private void populateEngineList() {
+		// Internal parser is always available
+		final var internalParser = new TreeParser();
+		System.out.println("Found engine: " + internalParser.getName());
+		parsers.put(internalParser.getName(), internalParser);
+		
+		// Initialize script engines
+		final var m = new ScriptEngineManager();
+		for (var factory : m.getEngineFactories()) {
+			final String name = factory.getNames().getFirst();
+			final Parser parser = new ScriptParser(name, factory.getScriptEngine());
+			System.out.println("Found engine: " + name);
+			parsers.put(name, parser);
+		}
 	}
 }
