96f019ae7ffd8a080bfc890ba6871fbf3cbc55f3
Subhraman Sarkar <suvrax@gmail.com>
2025-05-27 08:12:20 +0530

    Add Unit Test for TreeParser

---
+++ src/test/java/com/babai/ssplot/test/ParserTest.java
@@ -0,0 +1,29 @@
+package com.babai.ssplot.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.babai.ssplot.math.system.parser.internal.tree.TreeParser;
+
+class ParserTest {
+	private static TreeParser parser;
+	
+	@BeforeAll
+	static void initParser() {
+		parser = new TreeParser();
+	}
+	
+	@Test
+	void testBasicOperations() {
+		assertEquals(parser.parse("2 + 3").evalAt(Map.of()), 5, "2 + 3 != 5");
+		assertEquals(parser.parse("4 - 3").evalAt(Map.of()), 1, "4 - 3 != 1");
+		assertEquals(parser.parse("2 * 3").evalAt(Map.of()), 6, "2 * 3 != 6");
+		assertEquals(parser.parse("6 / 3").evalAt(Map.of()), 2, "6 / 3 != 2");
+		assertEquals(parser.parse("2 ^ 3").evalAt(Map.of()), 8, "2 ^ 3 != 8");
+	}
+
+}
