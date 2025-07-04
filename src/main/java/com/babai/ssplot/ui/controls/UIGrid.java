3652f65c37211b5bc0374d83a7111e49b5513b86
Subhraman Sarkar <suvrax@gmail.com>
2025-07-04 17:49:46 +0530

    DUI: introduce UIGrid
    a declarative wrapper around a JPanel with GridBagLayout

---
+++ src/main/java/com/babai/ssplot/ui/controls/UIGrid.java
@@ -0,0 +1,58 @@
+package com.babai.ssplot.ui.controls;
+
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+
+public class UIGrid extends JPanel {
+	private GridBagConstraints gbc;
+	
+	public UIGrid() {
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		setLayout(new GridBagLayout());
+	}
+	
+	public UIGrid anchor(int anchor) {
+		gbc.anchor = anchor;
+		return this;
+	}
+	
+	public UIGrid fill(int fill) {
+		gbc.fill = fill;
+		return this;
+	}
+	
+	public UIGrid insets(Insets insets) {
+		gbc.insets = insets;
+		return this;
+	}
+	
+	public UIGrid weightx(double weightx) {
+		gbc.weightx = weightx;
+		return this;
+	}
+	
+	public UIGrid weighty(double weighty) {
+		gbc.weighty = weighty;
+		return this;
+	}
+	
+	public UIGrid row() {
+		gbc.gridx = 0;
+		gbc.weightx = 0;
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridy++;
+		return this;
+	}
+	
+	public UIGrid column(Component comp) {
+		add(comp, gbc);
+		gbc.gridx++;
+		return this;
+	}
+}
