037237397a5d5a3c6d2aa9dd87b6df9da585b55a
Subhraman Sarkar <suvrax@gmail.com>
2025-08-08 09:53:21 +0530

    fixup, more diagonistics

--- packaging/appimage/package.sh
+++ packaging/appimage/package.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
+#!/bin/bash -xe
+
+set -euo pipefail
 
 # create directory structure
 ./link.sh
 mkdir -p SSPlot.AppDir/usr/share/ssplot
-cp -v "../../jar/ssplot.jar" "SSPlot.AppDir/usr/share/ssplot/"
+cp "../../jar/ssplot.jar" "SSPlot.AppDir/usr/share/ssplot/"
 cp ssplot SSPlot.AppDir/usr/bin/
 cp AppRun SSPlot.AppDir/
 cp ssplot.png SSPlot.AppDir/
 cp ssplot.desktop SSPlot.AppDir/
 
 # download appimagetool
-wget -nc https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+wget -O appimagetool-x86_64.AppImage https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+pwd && ls -lh
 chmod +x appimagetool-x86_64.AppImage
 ./appimagetool-x86_64.AppImage --appimage-extract
 
