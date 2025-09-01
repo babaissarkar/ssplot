6deb595578a96fc524d1e2af81c8ec43c5efaebf
Subhraman Sarkar <suvrax@gmail.com>
2025-09-01 09:03:32 +0530

    Use Maven Wrapper

--- generate_all.sh
+++ generate_all.sh
@@ -2,10 +2,10 @@
 # Copyright 2023-2025 Subhraman Sarkar
 # AppImageTool and any other required build programs
 # should exist on PATH
-# build-deps : maven, markdown
+# build-deps : markdown
 VERSION=2.3.1
-echo "Running Maven: Compile and generate DEB"
-mvn -B -X clean package jpackage:jpackage@linux
+echo "Running Maven: Compile and generate packages"
+./mvnw -B -X clean package jpackage:jpackage@linux
 echo "Generating Docs"
 markdown README.md > README.html
 echo "Creating AppImage"
